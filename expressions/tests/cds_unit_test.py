21fcd7b19cd5515f093c88f39830c398416d34e3
Greg McClement <greg.mcclement@gmail.com>
2017-05-22 23:35:52 -0700

    wip - doing the tests for cds

--- expressions/tests/cds_unit_test.py
+++ expressions/tests/cds_unit_test.py
@@ -11,3 +11,16 @@ class TddTestCDS(unittest.TestCase):
   def test_create(self):
     assert( CDS(self.session, 1,1) )
 
+  def test_session_is_set(self):
+    self.assertEqual(self.session, CDS(self.session, 1, 1).session)
+
+  def test_length_is_set(self):
+    self.assertEqual(23, CDS(self.session, 23, 1).length)
+
+  def test_depth_is_set(self):
+    self.assertEqual(42, CDS(self.session, 23, 42).depth)
+
+  def test_current_empty(self):
+    cds = CDS(self.session, 2, 4)
+    self.session.run(tf.global_variables_initializer())
+    self.assertEqual([], cds.current())
