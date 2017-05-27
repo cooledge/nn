19dfdd15d44254929394076c8181d28d1d271558
Greg McClement <greg.mcclement@gmail.com>
2017-05-27 09:37:54 -0700

    Layer 0 to layer 1 test complete. Start on layer 0 to layer 2 (ie dup the layer 0 to layer 1 test)

--- expressions/tests/cds_unit_test.py
+++ expressions/tests/cds_unit_test.py
@@ -43,131 +43,239 @@ class TddTestCDS(unittest.TestCase):
     cds.initialize(["one", "two", "three"])
     self.assertEqual([((0, 0), "one"), ((0, 1), "two"), ((0, 2), "three")], cds.current())
 
-  def test_current_joins_three_one_edge_case1(self):
+  def check_current_joins_three_one_edge_case1(self, next_row):
     cds = CDS(self.session, 3, 2)
     self.session.run(tf.global_variables_initializer())
     cds.initialize(["one", "two", "three"])
-    cds.joins([(0, 0)], (1, 0), "next")
-    self.assertEqual([((1, 0), "next"), ((0, 1), "two"), ((0, 2), "three")], cds.current())
+    cds.joins([(0, 0)], (next_row, 0), "next")
+    self.assertEqual([((next_row, 0), "next"), ((0, 1), "two"), ((0, 2), "three")], cds.current())
 
-  def test_current_joins_three_one_edge_case2(self):
-    cds = CDS(self.session, 3, 2)
-    self.session.run(tf.global_variables_initializer())
-    cds.initialize(["one", "two", "three"])
-    cds.joins([(0, 1)], (1, 0), "next")
-    self.assertEqual([((0, 0), "one"), ((1, 0), "next"), ((0, 2), "three")], cds.current())
+  def test_current_joins_three_one_edge_case1_next_1(self):
+    self.check_current_joins_three_one_edge_case1(1)
 
-  def test_current_joins_three_one_edge_case3(self):
+  def test_current_joins_three_one_edge_case1_next_2(self):
+    self.check_current_joins_three_one_edge_case1(2)
+
+  def check_current_joins_three_one_edge_case2(self, next_row):
     cds = CDS(self.session, 3, 2)
     self.session.run(tf.global_variables_initializer())
     cds.initialize(["one", "two", "three"])
-    cds.joins([(0, 2)], (1, 0), "next")
-    self.assertEqual([((0, 0), "one"), ((1, 0), "next"), ((0, 1), "two")], cds.current())
+    cds.joins([(0, 1)], (next_row, 0), "next")
+    self.assertEqual([((0, 0), "one"), ((next_row, 0), "next"), ((0, 2), "three")], cds.current())
+
+  def test_current_joins_three_one_edge_case2_next_1(self):
+    self.check_current_joins_three_one_edge_case2(1)
+
+  def test_current_joins_three_one_edge_case2_next_2(self):
+    self.check_current_joins_three_one_edge_case2(2)
+
+  def check_current_joins_three_one_edge_case3(self, next_row):
+    cds = CDS(self.session, 3, 2)
+    self.session.run(tf.global_variables_initializer())
+    cds.initialize(["one", "two", "three"])
+    cds.joins([(0, 2)], (next_row, 0), "next")
+    self.assertEqual([((0, 0), "one"), ((next_row, 0), "next"), ((0, 1), "two")], cds.current())
+
+  def test_current_joins_three_one_edge_case3_next_1(self):
+    self.check_current_joins_three_one_edge_case3(1)
+
+  def test_current_joins_three_one_edge_case3_next_1(self):
+    self.check_current_joins_three_one_edge_case3(2)
+
+  def check_current_joins_three_one_edge_case4(self, next_row):
+    cds = CDS(self.session, 3, 2)
+    self.session.run(tf.global_variables_initializer())
+    cds.initialize(["one", "two", "three"])
+    cds.joins([(0, 0)], (next_row, 1), "next")
+    self.assertEqual([((0, 1), "two"), ((next_row, 1), "next"), ((0, 2), "three")], cds.current())
 
   def test_current_joins_three_one_edge_case4(self):
-    cds = CDS(self.session, 3, 2)
-    self.session.run(tf.global_variables_initializer())
-    cds.initialize(["one", "two", "three"])
-    cds.joins([(0, 0)], (1, 1), "next")
-    self.assertEqual([((0, 1), "two"), ((1, 1), "next"), ((0, 2), "three")], cds.current())
+    self.check_current_joins_three_one_edge_case4(1)
 
-  def test_current_joins_three_one_edge_case5(self):
-    cds = CDS(self.session, 3, 2)
-    self.session.run(tf.global_variables_initializer())
-    cds.initialize(["one", "two", "three"])
-    cds.joins([(0, 1)], (1, 1), "next")
-    self.assertEqual([((0, 0), "one"), ((1, 1), "next"), ((0, 2), "three")], cds.current())
+  def test_current_joins_three_one_edge_case4(self):
+    self.check_current_joins_three_one_edge_case4(2)
 
-  def test_current_joins_three_one_edge_case6(self):
+  def check_current_joins_three_one_edge_case5(self, next_row):
     cds = CDS(self.session, 3, 2)
     self.session.run(tf.global_variables_initializer())
     cds.initialize(["one", "two", "three"])
-    cds.joins([(0, 2)], (1, 1), "next")
-    self.assertEqual([((0, 0), "one"), ((0, 1), "two"), ((1, 1), "next")], cds.current())
+    cds.joins([(0, 1)], (next_row, 1), "next")
+    self.assertEqual([((0, 0), "one"), ((next_row, 1), "next"), ((0, 2), "three")], cds.current())
 
-  def test_current_joins_three_one_edge_case7(self):
-    cds = CDS(self.session, 3, 2)
-    self.session.run(tf.global_variables_initializer())
-    cds.initialize(["one", "two", "three"])
-    cds.joins([(0, 0)], (1, 2), "next")
-    self.assertEqual([((0, 1), "two"), ((0, 2), "three"), ((1, 2), "next")], cds.current())
+  def test_current_joins_three_one_edge_case5_next_1(self):
+    self.check_current_joins_three_one_edge_case5(1)
 
-  def test_current_joins_three_one_edge_case8(self):
-    cds = CDS(self.session, 3, 2)
-    self.session.run(tf.global_variables_initializer())
-    cds.initialize(["one", "two", "three"])
-    cds.joins([(0, 1)], (1, 2), "next")
-    self.assertEqual([((0, 0), "one"), ((0, 2), "three"), ((1, 2), "next")], cds.current())
+  def test_current_joins_three_one_edge_case5_next_2(self):
+    self.check_current_joins_three_one_edge_case5(2)
 
-  def test_current_joins_three_one_edge_case9(self):
+  def check_current_joins_three_one_edge_case6(self, next_row):
     cds = CDS(self.session, 3, 2)
     self.session.run(tf.global_variables_initializer())
     cds.initialize(["one", "two", "three"])
-    cds.joins([(0, 2)], (1, 2), "next")
-    self.assertEqual([((0, 0), "one"), ((0, 1), "two"), ((1, 2), "next")], cds.current())
+    cds.joins([(0, 2)], (next_row, 1), "next")
+    self.assertEqual([((0, 0), "one"), ((0, 1), "two"), ((next_row, 1), "next")], cds.current())
 
-  def test_current_joins_three_two_edges_case1(self):
-    cds = CDS(self.session, 3, 2)
-    self.session.run(tf.global_variables_initializer())
-    cds.initialize(["one", "two", "three"])
-    cds.joins([(0, 0), (0, 1)], (1, 0), "next")
-    self.assertEqual([((1, 0), "next"), ((0, 2), "three")], cds.current())
+  def test_current_joins_three_one_edge_case6_next_1(self):
+    self.check_current_joins_three_one_edge_case6(1)
 
-  def test_current_joins_three_two_edges_case2(self):
-    cds = CDS(self.session, 3, 2)
-    self.session.run(tf.global_variables_initializer())
-    cds.initialize(["one", "two", "three"])
-    cds.joins([(0, 0), (0, 1)], (1, 1), "next")
-    self.assertEqual([((1, 1), "next"), ((0, 2), "three")], cds.current())
+  def test_current_joins_three_one_edge_case6_next_2(self):
+    self.check_current_joins_three_one_edge_case6(2)
 
-  def test_current_joins_three_two_edges_case3(self):
+  def check_current_joins_three_one_edge_case7(self, next_row):
     cds = CDS(self.session, 3, 2)
     self.session.run(tf.global_variables_initializer())
     cds.initialize(["one", "two", "three"])
-    cds.joins([(0, 0), (0, 1)], (1, 2), "next")
-    self.assertEqual([((0, 2), "three"), ((1, 2), "next")], cds.current())
+    cds.joins([(0, 0)], (next_row, 2), "next")
+    self.assertEqual([((0, 1), "two"), ((0, 2), "three"), ((next_row, 2), "next")], cds.current())
 
-  def test_current_joins_three_two_edges_case4(self):
-    cds = CDS(self.session, 3, 2)
-    self.session.run(tf.global_variables_initializer())
-    cds.initialize(["one", "two", "three"])
-    cds.joins([(0, 0), (0, 2)], (1, 0), "next")
-    self.assertEqual([((1, 0), "next"), ((0, 1), "two")], cds.current())
+  def test_current_joins_three_one_edge_case7_next_1(self):
+    self.check_current_joins_three_one_edge_case7(1)
 
-  def test_current_joins_three_two_edges_case5(self):
-    cds = CDS(self.session, 3, 2)
-    self.session.run(tf.global_variables_initializer())
-    cds.initialize(["one", "two", "three"])
-    cds.joins([(0, 0), (0, 2)], (1, 1), "next")
-    self.assertEqual([((0, 1), "two"), ((1, 1), "next")], cds.current())
+  def test_current_joins_three_one_edge_case7_next_2(self):
+    self.check_current_joins_three_one_edge_case7(2)
 
-  def test_current_joins_three_two_edges_case6(self):
+  def check_current_joins_three_one_edge_case8(self, next_row):
     cds = CDS(self.session, 3, 2)
     self.session.run(tf.global_variables_initializer())
     cds.initialize(["one", "two", "three"])
-    cds.joins([(0, 0), (0, 2)], (1, 2), "next")
-    self.assertEqual([((0, 1), "two"), ((1, 2), "next")], cds.current())
+    cds.joins([(0, 1)], (next_row, 2), "next")
+    self.assertEqual([((0, 0), "one"), ((0, 2), "three"), ((next_row, 2), "next")], cds.current())
 
-  def test_current_joins_three_two_edges_case7(self):
-    cds = CDS(self.session, 3, 2)
-    self.session.run(tf.global_variables_initializer())
-    cds.initialize(["one", "two", "three"])
-    cds.joins([(0, 1), (0, 2)], (1, 0), "next")
-    self.assertEqual([((0, 0), "one"), ((1, 0), "next")], cds.current())
+  def test_current_joins_three_one_edge_case8_next_1(self):
+    self.check_current_joins_three_one_edge_case8(1)
 
-  def test_current_joins_three_two_edges_case8(self):
-    cds = CDS(self.session, 3, 2)
-    self.session.run(tf.global_variables_initializer())
-    cds.initialize(["one", "two", "three"])
-    cds.joins([(0, 1), (0, 2)], (1, 1), "next")
-    self.assertEqual([((0, 0), "one"), ((1, 1), "next")], cds.current())
+  def test_current_joins_three_one_edge_case8_next_2(self):
+    self.check_current_joins_three_one_edge_case8(2)
 
-  def test_current_joins_three_two_edges_case9(self):
+  def check_current_joins_three_one_edge_case9(self, next_row):
     cds = CDS(self.session, 3, 2)
     self.session.run(tf.global_variables_initializer())
     cds.initialize(["one", "two", "three"])
-    cds.joins([(0, 1), (0, 2)], (1, 2), "next")
-    self.assertEqual([((0, 0), "one"), ((1, 2), "next")], cds.current())
+    cds.joins([(0, 2)], (next_row, 2), "next")
+    self.assertEqual([((0, 0), "one"), ((0, 1), "two"), ((next_row, 2), "next")], cds.current())
+
+  def test_current_joins_three_one_edge_case9_next_1(self):
+    self.check_current_joins_three_one_edge_case9(1)
+
+  def test_current_joins_three_one_edge_case9_next_2(self):
+    self.check_current_joins_three_one_edge_case9(2)
+
+  def check_current_joins_three_two_edges_case1(self, next_row):
+    cds = CDS(self.session, 3, 2)
+    self.session.run(tf.global_variables_initializer())
+    cds.initialize(["one", "two", "three"])
+    cds.joins([(0, 0), (0, 1)], (next_row, 0), "next")
+    self.assertEqual([((next_row, 0), "next"), ((0, 2), "three")], cds.current())
+
+  def test_current_joins_three_two_edges_case1_next_1(self):
+    self.check_current_joins_three_two_edges_case1(1)
+
+  def test_current_joins_three_two_edges_case1_next_2(self):
+    self.check_current_joins_three_two_edges_case1(2)
+
+  def check_current_joins_three_two_edges_case2(self, next_row):
+    cds = CDS(self.session, 3, 2)
+    self.session.run(tf.global_variables_initializer())
+    cds.initialize(["one", "two", "three"])
+    cds.joins([(0, 0), (0, 1)], (next_row, 1), "next")
+    self.assertEqual([((next_row, 1), "next"), ((0, 2), "three")], cds.current())
+
+  def test_current_joins_three_two_edges_case2_next_1(self):
+    self.check_current_joins_three_two_edges_case2(1)
+
+  def test_current_joins_three_two_edges_case2_next_2(self):
+    self.check_current_joins_three_two_edges_case2(2)
+
+  def check_current_joins_three_two_edges_case3(self, next_row):
+    cds = CDS(self.session, 3, 2)
+    self.session.run(tf.global_variables_initializer())
+    cds.initialize(["one", "two", "three"])
+    cds.joins([(0, 0), (0, 1)], (next_row, 2), "next")
+    self.assertEqual([((0, 2), "three"), ((next_row, 2), "next")], cds.current())
+
+  def test_current_joins_three_two_edges_case3_next_1(self):
+    self.check_current_joins_three_two_edges_case3(1)
+
+  def test_current_joins_three_two_edges_case3_next_2(self):
+    self.check_current_joins_three_two_edges_case3(2)
+
+  def check_current_joins_three_two_edges_case4(self, next_row):
+    cds = CDS(self.session, 3, 2)
+    self.session.run(tf.global_variables_initializer())
+    cds.initialize(["one", "two", "three"])
+    cds.joins([(0, 0), (0, 2)], (next_row, 0), "next")
+    self.assertEqual([((next_row, 0), "next"), ((0, 1), "two")], cds.current())
+
+  def test_current_joins_three_two_edges_case4_next_1(self):
+    self.check_current_joins_three_two_edges_case4(1)
+
+  def test_current_joins_three_two_edges_case4_next_2(self):
+    self.check_current_joins_three_two_edges_case4(2)
+
+  def check_current_joins_three_two_edges_case5(self, next_row):
+    cds = CDS(self.session, 3, 2)
+    self.session.run(tf.global_variables_initializer())
+    cds.initialize(["one", "two", "three"])
+    cds.joins([(0, 0), (0, 2)], (next_row, 1), "next")
+    self.assertEqual([((0, 1), "two"), ((next_row, 1), "next")], cds.current())
+
+  def test_current_joins_three_two_edges_case5_next_1(self):
+    self.check_current_joins_three_two_edges_case5(1)
+
+  def test_current_joins_three_two_edges_case5_next_2(self):
+    self.check_current_joins_three_two_edges_case5(2)
+
+  def check_current_joins_three_two_edges_case6(self, next_row):
+    cds = CDS(self.session, 3, 2)
+    self.session.run(tf.global_variables_initializer())
+    cds.initialize(["one", "two", "three"])
+    cds.joins([(0, 0), (0, 2)], (next_row, 2), "next")
+    self.assertEqual([((0, 1), "two"), ((next_row, 2), "next")], cds.current())
+
+  def test_current_joins_three_two_edges_case6_next_1(self):
+    self.check_current_joins_three_two_edges_case6(1)
+
+  def test_current_joins_three_two_edges_case6_next_2(self):
+    self.check_current_joins_three_two_edges_case6(2)
+
+  def check_current_joins_three_two_edges_case7(self, next_row):
+    cds = CDS(self.session, 3, 2)
+    self.session.run(tf.global_variables_initializer())
+    cds.initialize(["one", "two", "three"])
+    cds.joins([(0, 1), (0, 2)], (next_row, 0), "next")
+    self.assertEqual([((0, 0), "one"), ((next_row, 0), "next")], cds.current())
+
+  def test_current_joins_three_two_edges_case7_next_1(self):
+    self.check_current_joins_three_two_edges_case7(1)
+
+  def test_current_joins_three_two_edges_case7_next_2(self):
+    self.check_current_joins_three_two_edges_case7(2)
+
+  def check_current_joins_three_two_edges_case8(self, next_row):
+    cds = CDS(self.session, 3, 2)
+    self.session.run(tf.global_variables_initializer())
+    cds.initialize(["one", "two", "three"])
+    cds.joins([(0, 1), (0, 2)], (next_row, 1), "next")
+    self.assertEqual([((0, 0), "one"), ((next_row, 1), "next")], cds.current())
+
+  def test_current_joins_three_two_edges_case8_next_1(self):
+    self.check_current_joins_three_two_edges_case8(1)
+
+  def test_current_joins_three_two_edges_case8_next_2(self):
+    self.check_current_joins_three_two_edges_case8(2)
+
+  def check_current_joins_three_two_edges_case9(self, next_row):
+    cds = CDS(self.session, 3, 2)
+    self.session.run(tf.global_variables_initializer())
+    cds.initialize(["one", "two", "three"])
+    cds.joins([(0, 1), (0, 2)], (next_row, 2), "next")
+    self.assertEqual([((0, 0), "one"), ((next_row, 2), "next")], cds.current())
+
+  def test_current_joins_three_two_edges_case9_next_1(self):
+    self.check_current_joins_three_two_edges_case9(1)
+
+  def test_current_joins_three_two_edges_case9_next_2(self):
+    self.check_current_joins_three_two_edges_case9(2)
 
   def check_current_joins_three_three_edges_case1(self, to_column):
     cds = CDS(self.session, 3, 2)
