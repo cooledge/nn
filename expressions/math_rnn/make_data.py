d7ef01afa2488d3485efa63a956576d4a7fb49bc
Greg McClement <greg.mcclement@gmail.com>
2017-06-11 13:20:57 -0700

    I am working on a new new neural net to select operators for expression parsing. I am trying an RNN this time.
    code that build the training data

---
+++ expressions/math_rnn/make_data.py
@@ -0,0 +1,43 @@
+# build the test data
+
+import pdb
+import itertools
+
+operators = "+-*/^"
+
+input = open('input.txt', "w")
+output = open('output.txt', "w")
+
+op_to_priority = { '+': 1, '-': 1, '*': 2, '/': 3, '^':4 }
+
+ops = ""
+for op in op_to_priority:
+  ops += op
+
+def select_output(input):
+  priority = 0
+  for op in input:
+    if op_to_priority[op] > priority:
+      sel_op = op
+      priority = op_to_priority[op]
+  return sel_op
+
+inputs = op_to_priority.keys()
+
+def add_inputs(tuples, inputs):
+  for t in tuples:
+    ops = ""
+    for op in t:
+      ops += op
+    inputs += [ops]
+
+add_inputs(itertools.permutations(op_to_priority.keys(), 2), inputs)
+add_inputs(itertools.permutations(op_to_priority.keys(), 3), inputs)
+add_inputs(itertools.permutations(op_to_priority.keys(), 4), inputs)
+
+for op in inputs:
+  input.write("{0}\n".format(op))
+  output.write("{0}\n".format(select_output(op)))
+
+output.close()
+input.close()
