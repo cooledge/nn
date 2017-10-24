c4722f4cf9deedde756393a55ad26162c81a27f0
Greg McClement <greg.mcclement@gmail.com>
2017-10-23 22:15:29 -0700

    laptop needs new batter so checking this in they are WIP

---
+++ expressions/expressions7.py
@@ -0,0 +1,284 @@
+# this version has the current state of the parse represented in a NN
+# using the CDS data structure. 
+
+import tensorflow as tf
+import numpy as np
+import sys
+import pdb
+from hierarchy4 import HierarchyModel
+from cds2 import CDS
+from chain1 import ChainModel
+
+class ParserModel:
+
+  def __init__(self, priorities, default_priority, one_hot_inputs):
+    self.priorities = priorities
+    self.default_priority = default_priority
+    self.one_hot_inputs = one_hot_inputs
+    self.init_data()
+    self.get_preference_model()
+    #self.get_parser_model()
+
+  def setup(self, inputs):
+    self.get_parser_model(inputs)
+
+  def outputs(self):
+    return self.model_op_idx
+
+  def from_op_to_id(self, op):
+    try:
+      if op in self.op_to_id.keys():
+        return(self.op_to_id[op])
+      else:
+        return(self.op_to_id[default_priority])
+    except:
+      return(self.op_to_id[default_priority])
+     
+  def init_data(self):
+    self.id_to_op = {}
+    self.op_to_id = {}
+
+    for op in self.one_hot_inputs:
+      id = len(self.id_to_op)
+      self.id_to_op[id] = op
+      self.op_to_id[op] = id
+
+    self.number_of_ops = len(self.one_hot_inputs)
+    batch_size = len(self.priorities)
+    self.inputs = np.zeros((batch_size, self.number_of_ops))
+    self.labels = np.zeros((batch_size, self.number_of_ops))
+    batch_no = -1
+    for k, v in self.priorities:
+      batch_no = batch_no + 1
+      self.labels[batch_no][self.from_op_to_id(k)] = 1.0
+      self.inputs[batch_no][self.from_op_to_id(v)] = 1.0
+
+  def get_preference_model(self):
+    self.model_inputs = tf.placeholder(tf.float32, shape=(None, self.number_of_ops), name="expression_inputs")
+    self.model_labels = tf.placeholder(tf.float32, shape=(None, self.number_of_ops), name="expression_labels")
+    self.model_w = tf.Variable(tf.zeros((self.number_of_ops, self.number_of_ops)))
+    self.model_b = tf.Variable(tf.zeros((self.number_of_ops)))
+    model_logits =  tf.matmul(self.model_inputs, self.model_w) + self.model_b
+    self.model_loss = tf.losses.softmax_cross_entropy(self.model_labels, model_logits)
+    self.model_predict = tf.nn.softmax(model_logits)
+    model_optimizer = tf.train.AdamOptimizer(0.05)
+    self.model_train_op = model_optimizer.minimize(self.model_loss)
+
+  def get_parser_model(self, model_inputs = None):
+    if model_inputs == None:
+      model_inputs = self.model_inputs
+    model_logits =  tf.matmul(model_inputs, self.model_w) + self.model_b
+    # predict what is lower priority
+    self.model_predict = tf.nn.softmax(model_logits)
+    self.model_combined = tf.squeeze(tf.reshape(tf.reduce_sum(tf.split(self.model_predict, self.number_of_ops, axis=1), 1), (1, -1)))
+    self.model_reduced = tf.nn.relu(model_inputs - self.model_combined)
+    self.model_op_idx = tf.arg_max(tf.reduce_sum(self.model_reduced, 1), 0)
+
+  def train(self, session, epochs = 50):
+    for epoch in range(epochs):
+      feed_dict = { self.model_inputs: self.inputs, self.model_labels: self.labels }
+      _, loss = session.run([self.model_train_op, self.model_loss], feed_dict)
+      print("Epoch {0} loss {1}".format(epoch, loss))
+
+  def model_op_ids(self):
+    return self.model_op_ids
+
+  def get_op(self, expression):
+    inputs = np.zeros((len(expression), self.number_of_ops))
+    for i in range(len(expression)):
+      inputs[i][self.from_op_to_id(expression[i])] = 1
+
+    return session.run(self.model_op_idx, feed_dict = { self.model_inputs: inputs })
+
+priorities = [
+  ('preposition', 'article'),
+  ('infix', 'preposition'),
+  ('constant', 'infix'),
+  ('0', 'constant')
+]
+
+words = [
+  ('constant', 'constant'),
+  ('to', 'preposition'),
+  ('from', 'preposition'),
+  ('move', 'infix'),
+  ('bought', 'infix'),
+  ('a', 'article'),
+  ('the', 'article'),
+
+  # top off the loop
+  ('article', 'article'),
+  ('preposition', 'preposition'),
+  ('infix', 'infix')
+]
+
+one_hot_spec = ['constant', 'preposition', 'infix', 'article', '0']
+hierarchy_model = HierarchyModel(words, 'constant', one_hot_spec)
+
+parser_model = ParserModel(priorities, 'constant', one_hot_spec)
+
+def get_next_level(elements):
+  return max([element[0] for element in elements]) + 1
+ 
+def apply_move_chess_piece(op_idx, expression, cds):
+  op = expression[op_idx]
+
+  result = do_op(op_idx, [], "action", ["piece", "square"], cds, 
+              {
+                op_idx: lambda e: "move_chess_piece",
+                op_idx+2: lambda to: to["thing"]
+              })
+
+  before = expression[:op_idx]
+  after = expression[op_idx+3:]
+  return before + [result] + after
+
+def apply_bought(op_idx, expression, cds):
+  op = expression[op_idx]
+
+  result = do_op(op_idx, ["buyer"], "action", ["thing"], cds, {op_idx: lambda e: "buy"})
+
+  before = expression[:op_idx-1]
+  after = expression[op_idx+2:]
+  return before + [result] + after
+
+def do_op(op_idx, before_tags, op_tag, after_tags, cds, overrides = {}):
+  expression2 = cds.current()
+
+  tag_to_index = { op_tag: op_idx }
+  for idx, after_tag in enumerate(after_tags):
+    tag_to_index[after_tag] = op_idx + idx + 1
+  for idx, before_tag in enumerate(before_tags):
+    tag_to_index[before_tag] = op_idx - idx - 1
+
+  result = {}
+  for tag in tag_to_index:
+    idx = tag_to_index[tag]
+    if idx in overrides:
+      result[tag] = overrides[idx](expression2[idx][1])
+    else:
+      result[tag] = expression2[idx][1] 
+
+  op_pos = expression2[op_idx][0]
+  joins = [ expression2[idx][0] for idx in tag_to_index.values() ]
+  next_level = get_next_level(joins)
+  cds.joins(joins, (next_level, op_pos[1]), result)
+  return result
+
+def apply_preposition(op_idx, expression, cds):
+  op = expression[op_idx]
+  result = do_op(op_idx, [], "preposition", ["thing"], cds)
+ 
+  before = expression[:op_idx]
+  after = expression[op_idx+2:]
+  r = expression[op_idx + 1]
+  result1 = before + [result] + after
+  return result1
+
+def apply_done(op_idx, expression, cds):
+  raise "done"
+
+def increment_depth(tuple):
+  return (tuple[0]+1, tuple[1])
+
+def apply_article(op_idx, expression, cds):
+  op = expression[op_idx]
+  result = do_op(op_idx, [], "determiner", ["thing"], cds)
+  
+  before = expression[:op_idx]
+  after = expression[op_idx+2:]
+  r = expression[op_idx + 1]
+  return before + [result] + after
+
+op_to_apply = { 
+  "move": apply_move_chess_piece, 
+  "bought": apply_bought, 
+  "to": apply_preposition, 
+  "a": apply_article, 
+  "the": apply_article,
+  "constant": apply_done
+}
+
+'''
+can you run neural nets in reverse?
+m1 implies what for arg1 and arg2 arg1 and implies what for m
+output of a neural net is one node for each type -> then convert that to a one hot vector ala hierarchy
+
+m1 m2 m3   +    arg1.1 arg1.2   + arg2.1 arg2.2 arg2.3
+  move a car by driving
+  move a chess piece with a hand
+  move from one house to another
+
+move1 after to -> together or not
+present + types
+
+nueral net for numbers
+proper names
+
+the processing loops sets weights in a fully connection multilayer net. That is used as input to the loop
+that is you cool data structure (CDS)
+
+expression -> CDS -> expression -f-> CDS -> expression -f-> CDS
+
+'''
+
+if sys.version_info.major == 2:
+  def read_string(message):
+    return raw_input(message)
+else:
+  def read_string(message):
+    return input(message)
+
+session = tf.Session()
+session.run(tf.global_variables_initializer())
+
+chain_model = ChainModel(hierarchy_model, parser_model)
+chain_model.train(session)
+
+class Evaluator:
+
+  def evaluate(string):
+    max_length = 10
+    max_depth = 6
+    session_cds = tf.Session()
+    cds = CDS(session_cds, max_length, max_depth)
+    session_cds.run(tf.global_variables_initializer())
+    expression = string.split()
+
+    cds.initialize(expression)
+    cds.show() 
+
+    print("Input Expression: {0}".format(expression))
+    while len(expression) > 1:
+      op_idx = chain_model.apply(session, expression)
+      op = expression[op_idx]
+
+      try: 
+        op_to_apply[op](op_idx, expression, cds) 
+        expression = [t[1] for t in cds.current()]
+      except:
+        break
+
+    return [t[1] for t in cds.current()]
+
+'''
+expression1 = evaluate("move the boat to the island")
+expected1 = [{'action': 'move_chess_piece', 'piece': {'thing': 'boat', 'determiner': 'the'}, 'square': {'thing': 'island', 'determiner': 'the'}}]
+assert expression1 == expected1
+
+expression2 = evaluate("move t1 to t2")
+expected2 = [{'action': 'move_chess_piece', 'piece': 't1', 'square': 't2'}]
+assert expression2 == expected2
+'''
+
+while True:
+  ex_string = read_string("Enter an sentence if you dare: ")
+  if ex_string == "":
+    break
+
+  expression = evaluate(ex_string)
+
+  for e in expression:
+    if isinstance(e, dict):
+      print("Output Expression: {0}".format(e))
+  
---
+++ expressions/math_rnn/expressions2_with_new_training.py
@@ -0,0 +1,168 @@
+# described is at http://gregmcclement.blogspot.ca/2017/03/parsing-natural-languages.html
+
+import tensorflow as tf
+import numpy as np
+import pdb
+
+priorities = [
+  ('to', 'a'),
+  ('to', 'the'),
+  ('move', 'to'),
+  ('bought', 'to'),
+  ('c', 'move'),
+  ('c', 'bought'),
+  ('0', 'c')
+]
+
+ops = ['move', 'bought', 'a', 'the', 'to', 'c', '0']
+
+id_to_op = {}
+op_to_id = {}
+for op in ops:
+  id = len(id_to_op)
+  id_to_op[id] = op
+  op_to_id[op] = id
+
+def from_op_to_id(op):
+  try:
+    if op in op_to_id.keys():
+      return(op_to_id[op])
+    else:
+      return(op_to_id['c'])
+  except:
+    return(op_to_id['c'])
+ 
+number_of_ops = len(ops)
+batch_size = len(priorities)
+inputs = np.zeros((batch_size, number_of_ops))
+labels = np.zeros((batch_size, number_of_ops))
+batch_no = -1
+for k, v in priorities:
+  batch_no = batch_no + 1
+  labels[batch_no][from_op_to_id(k)] = 1.0
+  inputs[batch_no][from_op_to_id(v)] = 1.0
+
+model_inputs = tf.placeholder(tf.float32, shape=(None, number_of_ops), name="inputs")
+# the select op
+model_outputs = tf.placeholder(tf.float32, shape=(1, number_of_ops), name="outputs")
+
+model_w = tf.get_variable("w", shape=(number_of_ops, number_of_ops), dtype=tf.float32)
+model_b = tf.get_variable("b", shape=(number_of_ops), dtype=tf.float32)
+model_logits =  tf.matmul(model_inputs, model_w) + model_b
+model_predict = tf.nn.softmax(model_logits)
+
+model_combined = tf.squeeze(tf.reshape(tf.reduce_sum(tf.split(model_predict, number_of_ops, axis=1), 1), (1, -1)))
+pdb.set_trace()
+model_reduced = tf.nn.relu(model_inputs - model_combined)
+model_loss = tf.losses.softmax_cross_entropy(model_outputs, model_reduced)
+model_optimizer = tf.train.AdamOptimizer(0.05)
+model_train = model_optimizer.minimize(model_loss)
+pdb.set_trace()
+model_op_idx = tf.arg_max(tf.reduce_sum(model_reduced, 1), 0)
+
+session = tf.Session()
+session.run(tf.global_variables_initializer())
+
+pdb.set_trace()
+epochs = 50
+for epoch in range(epochs):
+  for batch in range(len(inputs)):
+    feed_dict = { model_inputs: inputs[batch:batch+1], model_outputs: labels[batch:batch+1] }
+    _, loss = session.run([model_train, model_loss], feed_dict)
+    print("Epoch {0} loss {1}".format(epoch, loss))
+
+# check it
+
+for op in ops:
+  inputs = np.zeros((1, number_of_ops))
+  inputs[0][from_op_to_id(op)] = 1
+  predict, = session.run([model_predict], feed_dict = {model_inputs: inputs})
+  predict = predict[0]
+  found = []
+  for i in range(len(predict)):
+    if predict[i] > 0.25:
+      found.append("{0} - {1:0.2f}".format(id_to_op[i], predict[i]))
+
+  print("op {0} found {1}".format(op, found))
+
+'''
+   c+-*/(**)
+   
+   input:        000100    001000
+
+   predict:      111000    100000
+
+   combined:     211000 (sum across predict)
+
+   reduced:      -2-1-1000 -2-10100
+'''
+
+def get_op(expression):
+  inputs = np.zeros((len(expression), number_of_ops))
+  for i in range(len(expression)):
+    inputs[i][from_op_to_id(expression[i])] = 1
+  reduced, combined, op_idx = session.run([model_reduced, model_combined, model_op_idx], feed_dict = { model_inputs: inputs })
+
+  ''' 
+  # debug prints 
+  print("combined")
+  print(combined)
+  print("inputs")
+  print(inputs)
+  print("reduced")
+  print(reduced)
+  # end debug prints
+  '''
+
+  return op_idx
+
+while True:
+  ex_string = raw_input("Enter an sentence if you dare: ")
+  if ex_string == "":
+    break
+
+  expression = ex_string.split()
+  print("Input Expression: {0}".format(expression))
+  #while len(expression) > 1:
+  while len(expression) > 1:
+    op_idx = get_op(expression)
+    op = expression[op_idx]
+
+    # did all the real ops
+    if from_op_to_id(op) == 'c':
+      break;
+    
+    if op == 'bought':
+      before = expression[:op_idx-1]
+      after = expression[op_idx+2:]
+      l = expression[op_idx - 1]
+      r = expression[op_idx + 1]
+      result = { "action": "buy", "buyer": l, "thing": r }
+    elif op == 'move':
+      before = expression[:op_idx]
+      after = expression[op_idx+3:]
+      r = expression[op_idx + 1]
+      to = expression[op_idx + 2]
+      result = { "action": 'move', "thing": r, "to": to["thing"] }
+    elif op == 'to':
+      before = expression[:op_idx]
+      after = expression[op_idx+2:]
+      r = expression[op_idx + 1]
+      result = { "preposition": op, "thing": r }
+    elif op == 'a' or op == 'the':
+      before = expression[:op_idx]
+      after = expression[op_idx+2:]
+      r = expression[op_idx + 1]
+      result = { "determiner": op, "thing": r }
+    else:
+      break
+
+    expression = before + [result] + after
+
+  for e in expression:
+    if isinstance(e, dict):
+      print("Output Expression: {0}".format(e))
+  
+
+
+
