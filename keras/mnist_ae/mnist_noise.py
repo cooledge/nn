2542158e5a460a1fdbd3adb95c8b4beecb9a4dbc
Greg McClement <greg.mcclement@gmail.com>
2018-03-02 22:27:12 -0800

    finish that up

--- keras/mnist_ae/mnist_noise.py
+++ keras/mnist_ae/mnist_noise.py
@@ -1,6 +1,7 @@
 from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D
 from keras.models import Model, Sequential
 from keras.regularizers import l1, l2
+import matplotlib.pyplot as plt
 import pdb
 
 encoding_dim = 32
@@ -63,17 +64,38 @@ x_train = x_train.astype('float32') / 255.
 x_test = x_test.astype('float32') / 255.
 
 pdb.set_trace()
-shape = autoencoder.output.shape.as_list()[1:]
-x_train = np.reshape(x_train, tuple([len(x_train)]+shape))
-x_test = np.reshape(x_test, tuple([len(x_test)] + shape))
+#shape = autoencoder.output.shape.as_list()[1:]
+#x_train = np.reshape(x_train, tuple([len(x_train)]+shape))
+#x_test = np.reshape(x_test, tuple([len(x_test)] + shape))
+x_train = np.reshape(x_train, (len(x_train), 28, 28, 1))
+x_test = np.reshape(x_test, (len(x_test), 28, 28, 1))
 
 #x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
 #x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
 
+noise_factor = 0.5
+x_train_noisy = x_train + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_train.shape) 
+x_test_noisy = x_test + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_test.shape) 
+
+x_train_noisy = np.clip(x_train_noisy, 0., 1.)
+x_test_noisy = np.clip(x_test_noisy, 0., 1.)
+
+'''
+n = 10
+plt.figure(figsize=(20, 2))
+for i in range(n):
+  ax = plt.subplot(1, n, i+1)
+  plt.imshow(x_test_noisy[i].reshape(28, 28))
+  plt.gray()
+  ax.get_xaxis().set_visible(False)
+  ax.get_yaxis().set_visible(False)
+plt.show()
+'''
+
 print(x_train.shape)
 print(x_test.shape)
 
-autoencoder.fit(x_train, x_train,
+autoencoder.fit(x_train_noisy, x_train,
                 epochs=100,
                 batch_size=256,
                 shuffle=True,
@@ -83,17 +105,16 @@ autoencoder.fit(x_train, x_train,
 # note that we take them from the *test* set
 #encoded_imgs = encoder.predict(x_test)
 #decoded_imgs = decoder.predict(encoded_imgs)
-decoded_imgs = autoencoder.predict(x_test)
+decoded_imgs = autoencoder.predict(x_test_noisy)
 
 # use Matplotlib (don't ask)
-import matplotlib.pyplot as plt
 
 n = 10  # how many digits we will display
 plt.figure(figsize=(20, 4))
 for i in range(n):
     # display original
     ax = plt.subplot(2, n, i + 1)
-    plt.imshow(x_test[i].reshape(28, 28))
+    plt.imshow(x_test_noisy[i].reshape(28, 28))
     plt.gray()
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
