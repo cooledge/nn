d8c089bb5ed8641fd617ade7d7f262a2dbee5789
Greg McClement <greg.mcclement@gmail.com>
2018-03-22 17:44:12 -0700

    split out the cat training

--- swap/sae.py
+++ swap/sae.py
@@ -170,6 +170,7 @@ def show_graph(sess):
 
   def plot_image(col, image, cat):
     ax = plt.subplot(n_rows,n_cols,(4*col)+i+1)
+    plt.clf()
     plt.imshow(image) 
     ax.text(70, 50, cat, fontsize=15, backgroundcolor='w')
     no_axis(ax)
@@ -189,14 +190,16 @@ saver = tf.train.Saver()
 model_prob_cat = tf.nn.softmax(model_categorizer)
 model_output_cat = tf.placeholder(tf.float32, shape=[None, 2], name="model_output_cat")
 model_loss_cat = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=model_output_cat, logits=model_categorizer))
+model_optimizer_cat = tf.train.AdamOptimizer(learning_rate=5e-5, beta1=0.5, beta2=0.999)
+model_train_op_cat = model_optimizer_cat.minimize(model_loss_cat)
 
 model_loss_A = tf.reduce_mean(tf.keras.losses.mean_absolute_error(tf.layers.flatten(model_input), tf.layers.flatten(autoencoder_A)))
 model_optimizer_A = tf.train.AdamOptimizer(learning_rate=5e-5, beta1=0.5, beta2=0.999)
-model_train_op_A = model_optimizer_A.minimize(model_loss_A+model_loss_cat)
+model_train_op_A = model_optimizer_A.minimize(model_loss_A)
 
 model_loss_B = tf.reduce_mean(tf.keras.losses.mean_absolute_error(tf.layers.flatten(model_input), tf.layers.flatten(autoencoder_B)))
 model_optimizer_B = tf.train.AdamOptimizer(learning_rate=5e-5, beta1=0.5, beta2=0.999)
-model_train_op_B = model_optimizer_B.minimize(model_loss_B+model_loss_cat)
+model_train_op_B = model_optimizer_B.minimize(model_loss_B)
 
 combine_images = False
 
@@ -239,16 +242,27 @@ for epoch in range(epochs):
       end = (batch+1)*BATCH_SIZE
 
       placeholders = {
-        model_input: get_batch(indexes_A, start, end, timages_A),
         model_output_cat: onehot_A
       }
       loss_A, _ = sess.run([model_loss_A, model_train_op_A], placeholders)
 
       placeholders = {
-        model_input: get_batch(indexes_B, start, end, timages_B),
+        model_input: get_batch(indexes_A, start, end, timages_A),
+        model_output_cat: onehot_A
+      }
+      loss_cat_A, _ = sess.run([model_loss_cat, model_train_op_cat], placeholders)
+
+      placeholders = {
         model_output_cat: onehot_B
       }
       loss_B, _ = sess.run([model_loss_B, model_train_op_B], placeholders)
-    print("Step: {0} loss_a: {1} loss_b: {2}\r".format(step, loss_A, loss_B))
+
+      placeholders = {
+        model_input: get_batch(indexes_B, start, end, timages_B),
+        model_output_cat: onehot_B
+      }
+      loss_cat_B, _ = sess.run([model_loss_cat, model_train_op_cat], placeholders)
+
+    print("Step: {0} loss_a: {1}/{2} loss_b: {3}/{4}\r".format(step, loss_A, loss_cat_A, loss_B, loss_cat_B))
     show_graph(sess)
     saver.save(sess, SAVE_FILE)
