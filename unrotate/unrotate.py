d1342e81c3dd7af68e72320691e7ec49acf259a8
Greg McClement <greg.mcclement@gmail.com>
2018-03-31 15:20:07 -0700

    unrotate image and figure out the angle

--- unrotate/unrotate.py
+++ unrotate/unrotate.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import numpy as np
 import cv2
 import time
+import math
 import pdb
 import os.path
 import random
@@ -15,37 +16,50 @@ BATCH_SIZE = 32
 SAVE_DIR = 'models'
 SAVE_FILE = 'models/swap'
 
+def si(image):
+  cv2.imshow("", image)
+  cv2.waitKey(1000)
+
 def warp_images(images):
   warped_images = []
   target_images = []
+  rotations = []
   for image in images:
-    wi, ti = warp_image(image)
+    wi, ti, rotate = warp_image(image)
     warped_images.append(wi)
     target_images.append(ti)
+    rotations.append(rotate)
 
-  return warped_images, target_images
+  return warped_images, target_images, rotations
 
 def warp_image(image, coverage=160):
   image = cv2.resize(image, (256,256))
-  image = random_transform( image )
-  warped_img, target_img = random_warp( image, coverage )
-  return warped_img, target_img
+  transformed_image, rotation, flip = random_transform( image )
+  image, _, _ = random_transform( image, rotation=0, flip=flip )
+  warped_img, target_img, img = random_warp( transformed_image, image, coverage )
+  return warped_img, img, rotation
 
-def random_transform(image, rotation_range=10, zoom_range=0.05, shift_range=0.05, random_flip=0.4):
+def random_transform(image, rotation_range=90, rotation=None, zoom_range=0.00, shift_range=0.00, random_flip=0.5, flip=None):
   h, w = image.shape[0:2]
-  rotation = np.random.uniform(-rotation_range, rotation_range)
+  if rotation is None:
+    rotation = np.random.uniform(-rotation_range, rotation_range)
+  if rotation < 0:
+    rotation = 360 + rotation
   scale = np.random.uniform(1 - zoom_range, 1 + zoom_range)
   tx = np.random.uniform(-shift_range, shift_range) * w
   ty = np.random.uniform(-shift_range, shift_range) * h
   mat = cv2.getRotationMatrix2D((w // 2, h // 2), rotation, scale)
   mat[:, 2] += (tx, ty)
   result = cv2.warpAffine(image, mat, (w, h), borderMode=cv2.BORDER_REPLICATE)
-  if np.random.random() < random_flip:
+
+  if flip or np.random.random() < random_flip:
     result = result[:, ::-1]
-  return result
+    flip = True
+  return result, rotation, flip
 
 # get pair of random warped images from aligned face image
-def random_warp(image, coverage=160):
+def random_warp(transformed_image, image, coverage=160):
+  assert transformed_image.shape == (256, 256, 3)
   assert image.shape == (256, 256, 3)
   range_ = np.linspace(128 - coverage//2, 128 + coverage//2, 5)
   mapx = np.broadcast_to(range_, (5, 5))
@@ -57,14 +71,16 @@ def random_warp(image, coverage=160):
   interp_mapx = cv2.resize(mapx, (80, 80))[8:72, 8:72].astype('float32')
   interp_mapy = cv2.resize(mapy, (80, 80))[8:72, 8:72].astype('float32')
 
-  warped_image = cv2.remap(image, interp_mapx, interp_mapy, cv2.INTER_LINEAR)
+  warped_image = cv2.remap(transformed_image, interp_mapx, interp_mapy, cv2.INTER_LINEAR)
 
   src_points = np.stack([mapx.ravel(), mapy.ravel()], axis=-1)
   dst_points = np.mgrid[0:65:16, 0:65:16].T.reshape(-1, 2)
   mat = umeyama(src_points, dst_points, True)[0:2]
 
-  target_image = cv2.warpAffine(image, mat, (64, 64))
-  return warped_image, target_image
+  target_image = cv2.warpAffine(transformed_image, mat, (64, 64))
+  image = cv2.warpAffine(image, mat, (64, 64))
+
+  return warped_image, target_image, image
 
 def load_image(filepath):
   try:
@@ -120,17 +136,38 @@ def Decoder(layer):
 
   return layer
 
+def rotate_resolution():
+  return 5
+
+def number_of_rotates():
+  return math.ceil(360 / rotate_resolution())
+
+def rotate_to_one_hot_index(degrees):
+  rr = rotate_resolution()
+  return round(degrees/rr)
+
+def one_hot_index_to_rotate(index):
+  return index * rotate_resolution()
+
+def rotate_to_one_hot(degrees):
+  index = rotate_to_one_hot_index(degrees)
+  return [1 if i == index else 0 for i in range(number_of_rotates())]  
+
+def one_hot_to_rotate(one_hot):
+  return one_hot_index_to_rotate(np.argmax(one_hot))
+
 def Categorizer(layer):
-  return tf.layers.dense(layer, 2)
+  return tf.layers.dense(layer, number_of_rotates())
 
 def AutoEncoder():
   model_input = tf.placeholder(tf.float32, shape=[None, 64, 64, 3], name="signal_input")
   model_output = tf.placeholder(tf.float32, shape=[None, 64, 64, 3], name="signal_output")
   encoder = Encoder(model_input)
-  decoder_A = Decoder(encoder)
-  return decoder_A, model_input, model_output
+  decoder = Decoder(encoder)
+  categorizer = Categorizer(encoder)
+  return decoder, categorizer, model_input, model_output
 
-autoencoder_A, model_input, model_output = AutoEncoder()
+model_autoencoder, model_categorizer, model_input, model_output = AutoEncoder()
 trump_images = load_images("./photo/trump")
 cage_images = load_images("./photo/cage")
 
@@ -145,15 +182,19 @@ plt.figure(figsize=(n_rows,n_cols))
 n_images = 4
 
 def show_graph(sess):
-  def predict(images, indexes, autoencoder):
-    placeholders ={ 
-        model_input: images
-        }
+  def predict(images, autoencoder):
+    placeholders ={ model_input: images }
     return sess.run(autoencoder, placeholders)
 
-  A_images = [warp_image(cage_images[indexes_A[i]])[1] for i in range(len(cage_images))]
-  raw_cage = A_images[0:4]
-  decoded_cage = predict(A_images[0:4], indexes_A, autoencoder_A)
+  def predict_cat(images, categorizer):
+    placeholders = { model_input: images }
+    one_hots = sess.run(model_prob_cat, placeholders)
+    return [one_hot_to_rotate(one_hot) for one_hot in one_hots]
+
+  test_images = inputs[0:4]
+  test_rotations = rotations[0:4]
+  decoded_cage = predict(test_images[0:4], model_autoencoder)
+  decoded_angles = predict_cat(test_images[0:4], model_categorizer)
 
   def no_axis(ax):
     ax.get_xaxis().set_visible(False)
@@ -164,21 +205,33 @@ def show_graph(sess):
     plt.imshow(image) 
     no_axis(ax)
 
+  def plot_rotate(col, rotate):
+    ax = plt.subplot(n_rows,n_cols,(4*col)+i+1)
+    ax.text(0, 0, str(round(rotate))+"    ", fontsize=15, backgroundcolor='w')
+    no_axis(ax)
+
   for i in range(n_images):
-    plot_image(0, raw_cage[i])
+    plot_image(0, test_images[i])
     plot_image(1, decoded_cage[i])
+    plot_rotate(2, test_rotations[i])
+    plot_rotate(3, decoded_angles[i])
 
   plt.pause(0.001)
 
 saver = tf.train.Saver()
 
-model_loss_A = tf.reduce_mean(tf.keras.losses.mean_absolute_error(tf.layers.flatten(model_output), tf.layers.flatten(autoencoder_A)))
-model_optimizer_A = tf.train.AdamOptimizer(learning_rate=5e-5, beta1=0.5, beta2=0.999)
-model_train_op_A = model_optimizer_A.minimize(model_loss_A)
+model_prob_cat = tf.nn.softmax(model_categorizer)
+model_output_cat = tf.placeholder(tf.float32, shape=[None, number_of_rotates()], name="model_output_cat")
+model_loss_cat = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=model_output_cat, logits=model_categorizer))
+model_optimizer_cat = tf.train.AdamOptimizer(learning_rate=5e-5, beta1=0.5, beta2=0.999)
+model_train_op_cat = model_optimizer_cat.minimize(model_loss_cat)
 
-images_A = cage_images
-indexes_A = [i for i in range(len(images_A))]
-onehot_A = [[1,0] for _ in range(BATCH_SIZE)]
+model_loss = tf.reduce_mean(tf.keras.losses.mean_absolute_error(tf.layers.flatten(model_output), tf.layers.flatten(model_autoencoder)))
+model_optimizer = tf.train.AdamOptimizer(learning_rate=5e-5, beta1=0.5, beta2=0.999)
+model_train_op = model_optimizer.minimize(model_loss)
+
+images = cage_images
+indexes = [i for i in range(len(images))]
 
 def get_batch(indexes, start, end, x):
   value = [x[indexes[i]] for i in range(start, end)]
@@ -186,7 +239,7 @@ def get_batch(indexes, start, end, x):
 
 epochs = 10000
 steps = 50
-batches = len(images_A) // BATCH_SIZE
+batches = len(images) // BATCH_SIZE
 
 sess = tf.Session()
 
@@ -195,27 +248,30 @@ saved_model_path = tf.train.latest_checkpoint(SAVE_DIR)
 if False and saved_model_path:
   saver.restore(sess, saved_model_path)
 
-show_graph(sess)
+#show_graph(sess)
 
 last_time = time.time()
 for epoch in range(epochs):
   print("\nEpoch {0} seconds {1}".format(epoch, time.time()-last_time))
   last_time = time.time()
-  random.shuffle(indexes_A)
-  timages_A = [random_transform(image) for image in images_A]
+  random.shuffle(indexes)
+  timages = [random_transform(image) for image in images]
   for step in range(steps):
     for batch in range(batches):
       start = batch*BATCH_SIZE
       end = (batch+1)*BATCH_SIZE
 
-      batch_A = get_batch(indexes_A, start, end, images_A)
-      inputs_A, outputs_A = warp_images(batch_A)
+      batch = get_batch(indexes, start, end, images)
+      inputs, outputs, rotations = warp_images(batch)
+      one_hots = [rotate_to_one_hot(degrees) for degrees in rotations]
       placeholders = {
-        model_input: inputs_A,
-        model_output: outputs_A,
+        model_input: inputs,
+        model_output: outputs,
+        model_output_cat: one_hots
       }
-      loss_A, _ = sess.run([model_loss_A, model_train_op_A], placeholders)
+      # model_loss_cat, model_train_op_cat
+      loss, loss_cat, _, _ = sess.run([model_loss, model_loss_cat, model_train_op, model_train_op_cat], placeholders)
 
-    print("Step: {0} loss_a: {1}/{2}\r".format(step, loss_A))
+    print("Step: {0} loss_a: {1}/{2}\r".format(step, loss, loss_cat))
     show_graph(sess)
     saver.save(sess, SAVE_FILE)
