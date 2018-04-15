670bc452e073cf9b789094f9ee34a77e29eed721
Greg McClement <greg.mcclement@gmail.com>
2018-04-14 22:55:45 -0700

    rc stub

---
+++ moving/robotcar_stub.py
@@ -0,0 +1,17 @@
+﻿class RobotCar:
+
+  def forward(self):
+    print("Forward")
+    
+  def backward(self):
+    print("Backward")
+    
+  def left(self):
+    print("Left")
+    
+  def right(self):
+    print("Right")
+    
+  def stop(self):
+    print("Stop")
+
