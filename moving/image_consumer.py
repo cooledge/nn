91815b4d6e060724a257dd4b5a32d318880376d4
Greg McClement <greg.mcclement@gmail.com>
2018-04-20 22:28:32 -0700

    bug fixes

--- moving/image_consumer.py
+++ moving/image_consumer.py
@@ -4,6 +4,7 @@ import os
 import time
 from move import Predict
 import pdb
+import cv2
 
 def fn2idx(fn):
   return int(fn.strip(string.ascii_letters+"_."))
@@ -19,6 +20,10 @@ class FakePredict:
 
 consumers = [Predict()]
 
+def si(image):
+  cv2.imshow("", image)
+  cv2.waitKey(1)
+
 while True:
   time.sleep(0.1)
   files = os.listdir('./current')
@@ -27,13 +32,14 @@ while True:
   for file in files:
     idx2fn[fn2idx(file)] = file
 
-  idxs = idx2fn.keys()
+  idxs = list(idx2fn.keys())
   idxs.sort()
   for idx in idxs:
     try:
       filename = idx2fn[idx]
       filepath = to_path(filename)
       image = np.load(filepath)
+      si(image[0])
       os.remove(filepath)
       #print("Run against neural net {0}".format(filename))
       for consumer in consumers:
