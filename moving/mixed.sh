f9a8c0323f1186b9e8930450f64e9adda45e54f2
Greg McClement <greg.mcclement@gmail.com>
2018-04-25 20:36:08 -0700

    script for collecting all fix directs at once.

---
+++ moving/mixed.sh
@@ -0,0 +1,5 @@
+python get_data.py --direction forward --time 1000
+python get_data.py --direction backward --time 1000
+python get_data.py --direction left --time 1000
+python get_data.py --direction right --time 1000
+python get_data.py --direction stop --time 1000
