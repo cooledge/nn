771e5a5612ff21339a8699613447c65227381ee0
Greg McClement <greg.mcclement@gmail.com>
2018-07-06 07:37:02 -0700

    try only left right actions

--- robotcar/movement/get_data.py
+++ robotcar/movement/get_data.py
@@ -33,6 +33,7 @@ parser = argparse.ArgumentParser(description="Get data from the camera and motio
 parser.add_argument("--n_samples", type=int, default=1)
 parser.add_argument("--time", type=int, default=2000)
 parser.add_argument('--show', dest='show', default=False, action='store_true')
+parser.add_argument('--steps', type=int, default=0)
 parser.add_argument('--no-show', dest='show', action='store_false')
 parser.set_defaults(show=False)
 args = parser.parse_args()
@@ -77,23 +78,43 @@ td.get_frame()
 
 seq_len = params.N_ACTIONS
 def get_actions():
-  return ''.join([random.choice('fblrs') for _ in range(seq_len)])
+  return ''.join([random.choice(params.ACTIONS) for _ in range(seq_len)])
 
 def error_check(ret):
   return 0
 
 images = []
 start = time.time()  
+counter = 0
+# learn direction and how much time to move left or right
+step_dir = 'f'
+STEP_DURATION = 0.05
 if td.isOpened():
   for sample_no in range(args.n_samples):
     ret, before_frame = td.get_frame()
     error_check(ret)
 
+    if args.steps > 0:
+      if step_dir == 'f':
+        robotcar.move('f', STEP_DURATION)
+      else:
+        robotcar.move('b', STEP_DURATION)
+      robotcar.stop()
+      counter += 1
+      if counter >= args.steps:
+        counter = 0
+        if step_dir == 'b':
+          step_dir = 'f'
+        else:
+          step_dir = 'b'
+
+    time.sleep(1.0)
+
     actions = get_actions()
     print(actions)
     robotcar.move(actions, params.ACTION_DURATION)
     robotcar.stop()
-    time.sleep(0.5)
+    time.sleep(1.0)
        
     ret, after_frame = td.get_frame()
     error_check(ret)
@@ -101,6 +122,7 @@ if td.isOpened():
     # back to start
     robotcar.move(inverse(actions), params.ACTION_DURATION)
     robotcar.stop()
+    time.sleep(1.0)
 
     if args.show:
       cv2.imshow('before', before_frame)
@@ -110,12 +132,14 @@ if td.isOpened():
 
 print("Writing {0} example".format(len(images)))
 timestr = time.strftime("%Y%m%d%H%M%S")
+file_counter = 0
 for before, after, actions in images:
   def wf(counter, image):
-    filename = "{0}/{1}_{2}-{3}.jpg".format(data_dir, actions, timestr, counter)
+    filename = "{0}/{1}_{2}{3}-{4}.jpg".format(data_dir, actions, timestr, file_counter, counter)
     cv2.imwrite(filename, image)
   wf('1', before)
   wf('2', after)
+  file_counter += 1
 
 '''
 filespec = "{0}/{1}{2}*.jpg".format(data_dir, prefix, timestr, counter)
