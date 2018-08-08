8c634b43f6aa3322b9375b025eb20ebef1fb72fc
Greg McClement <greg.mcclement@gmail.com>
2018-08-08 07:30:38 -0700

    double checking my impl work same as otehr one

--- ae_text/conv1d_inverse.py
+++ ae_text/conv1d_inverse.py
@@ -16,7 +16,6 @@ def conv1d_inverse(layer, n_filters_out=1, kernel_size=2, strides=1, initializer
   kernels = tf.split(kernel, n_filters_out, axis=2)
   kernels = [tf.squeeze(kernel, axis=2) for kernel in kernels]
 
-  pdb.set_trace()
   outputs = [[tf.matmul(step, kernel) for step in steps] for kernel in kernels]
   outputs = [tf.concat(steps, axis=1) for steps in outputs]
   outputs = [tf.reshape(feature, (-1, seq_len*kernel_size, 1)) for feature in outputs]
@@ -54,9 +53,19 @@ if __name__ == '__main__':
     maxpool = session.run(model_maxpool, feed_dict=feed)
     print("maxpool {0}".format(maxpool))
 
-    model_upsample = conv1d_inverse(model_maxpool, 1, kernel_size=2, initializer=tf.constant_initializer(1.0))
+    n_filters_out = 2
+    kernel_size_out = 1
+    model_upsample = conv1d_inverse(model_maxpool, n_filters_out, kernel_size=kernel_size_out, initializer=tf.constant_initializer(1.0))
     upsample = session.run(model_upsample, feed_dict=feed)
-    print("upsample {0}".format(upsample))
+    print("upsample greg {0}".format(upsample))
+
+    # batch_size, batch_len, embedding_size
+    output_shape = [1, 5, n_filters_out]
+    # filter_width, output_channels, input_channels
+    l1_filter = tf.constant_initializer(1.0)((kernel_size_out, n_filters_out, n_filters))
+    model_upsample_ref = tf.contrib.nn.conv1d_transpose(model_maxpool, l1_filter, output_shape, stride=1)
+    upsample_ref = session.run(model_upsample_ref, feed_dict=feed)
+    print("upsample def {0}".format(upsample_ref))
 
     pdb.set_trace()
     pdb.set_trace()
