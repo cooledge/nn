051cb85b1d67884db90d3b6ce75a03dd237b2007
Greg McClement <greg.mcclement@gmail.com>
2018-09-03 10:00:06 -0700

    fix up mistake of not using state in the middle

--- spelling/words.py
+++ spelling/words.py
@@ -4,7 +4,8 @@ import pdb
 
 import numpy as np
 
-filename = './data/warandpeace.txt'
+#filename = './data/warandpeace.txt'
+filename = './data/small.txt'
 max_len = 20
 
 lines = []
@@ -21,7 +22,7 @@ for line in lines:
 
 id_to_char = [' '] + list(chars)
 char_to_id = { char:id for (id, char) in enumerate(id_to_char) }
-
+vocab_size = len(id_to_char)
 words = [list(word)[0:max_len] for word in words]
 
 def word_remove_letter(word, i):
@@ -76,10 +77,63 @@ outputs = [word_chars_to_ids(word) for word in outputs]
 inputs = tf.keras.preprocessing.sequence.pad_sequences(inputs, maxlen=max_len, padding='post', value=char_to_id[" "])
 outputs = tf.keras.preprocessing.sequence.pad_sequences(outputs, maxlen=max_len, padding='post', value=char_to_id[" "])
 
-pdb.set_trace()
 # word -> correct word
-'''
+
 model = keras.Sequential()
-model = model.add(keras.layers.Embedding(vocab_size, 16))
-model = model.add(keras.layers.
+# produce output of 128 
+model.add(keras.layers.LSTM(128, input_shape=(max_len, vocab_size)))
+# this is the output length not the input length
+model.add(keras.layers.RepeatVector(max_len))
+
+# By setting return_sequences to True, return not only the last output but
+# all the outputs so far in the form of (num_samples, timesteps,
+# output_dim). This is necessary as TimeDistributed in the below expects
+# the first dimension to be the timesteps.
+model.add(keras.layers.LSTM(128, return_sequences=True))
+
+model.add(keras.layers.TimeDistributed(keras.layers.Dense(vocab_size)))
+model.add(keras.layers.Activation('softmax'))
+
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+def id_to_one_hot(tid):
+  one_hot = np.zeros((vocab_size,))
+  one_hot[tid] = 1.0
+  return one_hot
+
+def output_to_one_hot(output):
+  return [id_to_one_hot(id) for id in output]
+
+def to_one_hot(values):
+  return np.array([output_to_one_hot(value) for value in values])
+
+outputs = to_one_hot(outputs)
+inputs = to_one_hot(inputs)
+model.fit(inputs, outputs, epochs=50, batch_size=4)
+
+predictions = model.predict_classes(inputs)
+
+def from_categorical(batch, id_to_token_map):
+  def sequence_from_categorical(sequence):
+    return [id_to_token_map[id] for id in sequence]
+  return [sequence_from_categorical(sequence) for sequence in batch]
+
+pdb.set_trace()
+predictions = from_categorical(predictions, id_to_char)
+'''
+def one_hot_to_id(one_hot):
+  pdb.set_trace()
+  return np.argmax(one_hot)
+
+def one_hot_output_to_ids(output):
+  return [one_hot_to_id(one_hot) for one_hot in output]
+predictions = [ one_hot_output_to_ids(output) for output in predictions]
+'''
+
+pdb.set_trace()
+
+'''
+model = model.add(keras.layers.LSTM(32))
+model = model.add(keras.layers.LSTM(8))
+model = model.add(keras.layers.TimeDistributed(Dense(32))
 '''
