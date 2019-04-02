621bff27fb213a4a1fa95d71fae579ab10e30b88
Greg McClement <greg.mcclement@gmail.com>
2019-04-01 21:23:27 -0700

    works for add. next up combine

--- summer/summer.py
+++ summer/summer.py
@@ -1,12 +1,15 @@
 import tensorflow as tf
+from tensorflow import keras
+import numpy as np
 import pdb
 
 # Input: 2 Numbers and Op (Add, Subtract, Multiply, Divide)
 # Output: the result of applying Op
 
-ADD = 0
-SUBTRACT = 1
-MULTIPLY = 2
+
+'''
+idea: neural net A has all of its nodes multiplied by the output node of another net. zero turns it off. use that as a way to combine
+'''
 
 def to_one_hot(n, v):
   oh = [0 for _ in range(n)]
@@ -15,16 +18,17 @@ def to_one_hot(n, v):
 
 x = []
 y = []
-for i in range(-10, 10):
-  for j in range(-10, 10):
-    for op in range(4):
-      pdb.set_trace()
-      x += [to_one_hot(20, i+10) + to_one_hot(20, j+10) + to_one_hot(4, op)]
-      if op == ADD:
-        y += [to_one_hot(200, i+j+100)]
-      elif op == SUBTRACT:
-        y += [to_one_hot(200, i-j+100)]
-      elif op == MULTIPLY:
-        y += [to_one_hot(200, i*j+100)]
+for i in range(0, 10):
+  for j in range(0, 10):
+    x += [to_one_hot(10, i) + to_one_hot(10, j)]
+    y += [to_one_hot(100, i+j)]
 
-pdb.set_trace()
+x = np.array(x)
+y = np.array(y)
+
+model = keras.Sequential()
+model.add(keras.layers.Dense(4000, input_shape=(10+10,)))
+model.add(keras.layers.Dense(100, activation=tf.nn.softmax))
+
+model.compile(optimizer=tf.train.AdamOptimizer(), loss='categorical_crossentropy', metrics=['accuracy'])
+model.fit(np.array(x), np.array(y), epochs=500)
