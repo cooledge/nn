674c26e0a82b1154727ea8c6264214ab0665aa12
Greg McClement <greg.mcclement@gmail.com>
2019-04-02 21:32:42 -0700

    WIP

--- summer/summer.py
+++ summer/summer.py
@@ -29,6 +29,20 @@ y = np.array(y)
 model = keras.Sequential()
 model.add(keras.layers.Dense(4000, input_shape=(10+10,)))
 model.add(keras.layers.Dense(100, activation=tf.nn.softmax))
+#model.add(keras.layers.Multiply(0))
 
 model.compile(optimizer=tf.train.AdamOptimizer(), loss='categorical_crossentropy', metrics=['accuracy'])
 model.fit(np.array(x), np.array(y), epochs=500)
+
+# https://github.com/keras-team/keras/issues/1344
+#switched = keras.Sequential(model.input, model.output)
+#switched.add(Lambda( lambda x : x*0))
+#switched = keras.Model(model.input, model.output)
+pdb.set_trace()
+
+sm = keras.models.clone_model(model)
+sm.set_weights(model.get_weights())
+sm.add(keras.layers.Lambda( lambda x : x*0))
+
+predict = model.predict([[x[0]]])
+spredict = sm.predict([[x[0]]])
