6fef916e65a2e2aca82e7f9596b8d52f2fcf8ee9
Greg McClement <greg.mcclement@gmail.com>
2019-05-11 11:41:44 -0700

    wip - make work with non-eager mode

--- ttt/d.py
+++ ttt/d.py
@@ -47,43 +47,44 @@ samples = tf.convert_to_tensor(samples)
 
 # samples (?, 9, 9)
 
-def setup(samples):
-  pdb.set_trace()
+def board_to_features(board):
+  board = tf.squeeze(board)
 
+  row1 = tf.convert_to_tensor([board[0], board[1], board[2]])
+  row2 = tf.convert_to_tensor([board[3], board[4], board[5]])
+  row3 = tf.convert_to_tensor([board[6], board[7], board[8]])
+
+  col1 = tf.convert_to_tensor([board[0], board[3], board[6]])
+  col2 = tf.convert_to_tensor([board[1], board[4], board[7]])
+  col3 = tf.convert_to_tensor([board[2], board[5], board[8]])
+
+  diag1 = tf.convert_to_tensor([board[0], board[4], board[8]])
+  diag2 = tf.convert_to_tensor([board[2], board[4], board[6]])
+
+# 3 squares by 1 feature
+
+  n_features = 2
+# (3, n_features)
+  feature = [1 for _ in range(n_features)] 
+  features = tf.constant([feature, feature, feature]) 
+
+# set trace
+  print(tf.linalg.matmul([row2], features))
+  components = [row1, row2, row3, col1, col2, col3, diag1, diag2]
+  feature_layer = [tf.linalg.matmul([component], features)[0] for component in components]
+  feature_layer = tf.convert_to_tensor(feature_layer)
+  feature_layer = tf.reshape(feature_layer, (len(components)*n_features,))
+      
+  return feature_layer
+
+def setup(samples):
   samples_of_boards = [ tf.split(sample[0], 9, 0) for sample in tf.split(samples, samples.shape[0])]
 
   sample_output = []
   for sample in samples_of_boards:
     boards = []
     for board in sample:
-      board = tf.squeeze(board)
-
-      row1 = tf.convert_to_tensor([board[0], board[1], board[2]])
-      row2 = tf.convert_to_tensor([board[3], board[4], board[5]])
-      row3 = tf.convert_to_tensor([board[6], board[7], board[8]])
-
-      col1 = tf.convert_to_tensor([board[0], board[3], board[6]])
-      col2 = tf.convert_to_tensor([board[1], board[4], board[7]])
-      col3 = tf.convert_to_tensor([board[2], board[5], board[8]])
-
-      diag1 = tf.convert_to_tensor([board[0], board[4], board[8]])
-      diag2 = tf.convert_to_tensor([board[2], board[4], board[6]])
-
-      # 3 squares by 1 feature
-     
-      n_features = 2
-      # (3, n_features)
-      feature = [1 for _ in range(n_features)] 
-      features = tf.constant([feature, feature, feature]) 
-
-      # set trace
-      print(tf.linalg.matmul([row2], features))
-      components = [row1, row2, row3, col1, col2, col3, diag1, diag2]
-      feature_layer = [tf.linalg.matmul([component], features)[0] for component in components]
-      feature_layer = tf.convert_to_tensor(feature_layer)
-      feature_layer = tf.reshape(feature_layer, (len(components)*n_features,))
-          
-      boards.append(feature_layer)
+      boards.append(board_to_features(board))
     sample_output.append(boards)
 
   sample_output = tf.convert_to_tensor(sample_output)
