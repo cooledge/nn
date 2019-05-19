8a17fdf3eb25027da462ad78df26664f293ff1c5
Greg McClement <greg.mcclement@gmail.com>
2019-05-19 10:58:46 -0700

    well it wasn't convert_to_tensor that the problem so the search continues

--- ttt/ttt.py
+++ ttt/ttt.py
@@ -197,31 +197,35 @@ class TTTLayer(tf.keras.layers.Layer):
     self.kernel = []
     self.kernel = self.add_variable("TTTLayer_kernel", shape=[3, self.n_features])
 
+  def f(self, t):
+    return tf.reshape(t, (1, self.n_embedding))
+
   def board_to_features(self, board):
-    row1 = tf.convert_to_tensor([board[0], board[1], board[2]])
-    row2 = tf.convert_to_tensor([board[3], board[4], board[5]])
-    row3 = tf.convert_to_tensor([board[6], board[7], board[8]])
 
-    col1 = tf.convert_to_tensor([board[0], board[3], board[6]])
-    col2 = tf.convert_to_tensor([board[1], board[4], board[7]])
-    col3 = tf.convert_to_tensor([board[2], board[5], board[8]])
+    row1 = tf.concat([TTTLayer.f(self, board[0]), TTTLayer.f(self, board[1]), TTTLayer.f(self, board[2])], 0)
+    row2 = tf.concat([TTTLayer.f(self, board[3]), TTTLayer.f(self, board[4]), TTTLayer.f(self, board[5])], 0)
+    row3 = tf.concat([TTTLayer.f(self, board[6]), TTTLayer.f(self, board[7]), TTTLayer.f(self, board[8])], 0)
 
-    diag1 = tf.convert_to_tensor([board[0], board[4], board[8]])
-    diag2 = tf.convert_to_tensor([board[2], board[4], board[6]])
+    col1 = tf.concat([TTTLayer.f(self, board[0]), TTTLayer.f(self, board[3]), TTTLayer.f(self, board[6])], 0)
+    col2 = tf.concat([TTTLayer.f(self, board[1]), TTTLayer.f(self, board[4]), TTTLayer.f(self, board[7])], 0)
+    col3 = tf.concat([TTTLayer.f(self, board[2]), TTTLayer.f(self, board[5]), TTTLayer.f(self, board[8])], 0)
+
+    diag1 = tf.concat([TTTLayer.f(self, board[0]), TTTLayer.f(self, board[4]), TTTLayer.f(self, board[8])], 0)
+    diag2 = tf.concat([TTTLayer.f(self, board[2]), TTTLayer.f(self, board[4]), TTTLayer.f(self, board[6])], 0)
 
     # 3 squares by 1 feature
 
     # (3, n_features)
-    feature = [1.0 for _ in range(self.n_features)]
-    features = tf.constant([feature]*3*self.n_embedding)
-    pdb.set_trace()
+    #feature = [1.0 for _ in range(self.n_features)]
+    #features = tf.constant([feature]*3*self.n_embedding)
+    #pdb.set_trace()
     features = tf.random.uniform((3*self.n_embedding, self.n_features));
 
 # set trace
     #print(tf.linalg.matmul([row2], features))
     components = [row1, row2, row3, col1, col2, col3, diag1, diag2]
     feature_layer = [tf.linalg.matmul([tf.reshape(component, (3*self.n_embedding,))], features)[0] for component in components]
-    feature_layer = tf.convert_to_tensor(feature_layer)
+    feature_layer = tf.concat(feature_layer, 0)
     feature_layer = tf.reshape(feature_layer, (len(components)*self.n_features,))
 
     return feature_layer
@@ -235,47 +239,6 @@ class TTTLayer(tf.keras.layers.Layer):
   def call(self, samples):
     output = tf.map_fn(lambda sample: TTTLayer.sample_to_features(self, sample), samples)
     return output
-
-  def call2(self, samples):
-    pdb.set_trace()
-    samples_of_boards = [ tf.split(sample[0], 9, 0) for sample in tf.split(samples, samples.shape[0])]
-
-    sample_output = []
-    for sample in samples_of_boards:
-      boards = []
-      for board in sample:
-        board = tf.squeeze(board)
-        row1 = tf.convert_to_tensor([board[0], board[1], board[2]])
-        row2 = tf.convert_to_tensor([board[3], board[4], board[5]])
-        row3 = tf.convert_to_tensor([board[6], board[7], board[8]])
-
-        col1 = tf.convert_to_tensor([board[0], board[3], board[6]])
-        col2 = tf.convert_to_tensor([board[1], board[4], board[7]])
-        col3 = tf.convert_to_tensor([board[2], board[5], board[8]])
-
-        diag1 = tf.convert_to_tensor([board[0], board[4], board[8]])
-        diag2 = tf.convert_to_tensor([board[2], board[4], board[6]])
-
-        # 3 squares by 1 feature
-
-        # (3, n_features)
-        #feature = [1.0 for _ in range(self.n_features)]
-        #features = tf.constant([feature, feature, feature])
-        features = tf.random.uniform((1,3));
-
-        # set trace
-        print(tf.linalg.matmul([row2], features))
-        components = [row1, row2, row3, col1, col2, col3, diag1, diag2]
-        feature_layer = [tf.linalg.matmul([component], features)[0] for component in components]
-        feature_layer = tf.convert_to_tensor(feature_layer)
-        feature_layer = tf.reshape(feature_layer, (len(components)*self.n_features,))
-
-        boards.append(feature_layer)
-
-      sample_output.append(boards)
-
-    sample_output = tf.convert_to_tensor(sample_output)
-    return sample_output
     
 try: 
   model = keras.models.load_model('model')
