b3e728c10ac53f4bd348851a3ddf31698638b7dd
Greg McClement <greg.mcclement@gmail.com>
2019-06-08 10:43:34 -0700

    move to other machine

--- resize/resize.py
+++ resize/resize.py
@@ -13,7 +13,6 @@ def random_string(slen):
 FONT_START = 4
 FONT_SIZE_RANGE = range(FONT_START, 43)
 TARGET_FONT_SIZE = 16
-N_IMAGES = 1000
 DATA_DIR = './data'
 
 if not os.path.exists(DATA_DIR):
@@ -25,13 +24,15 @@ def get_font(size):
 
 font_y = get_font(TARGET_FONT_SIZE)
 
-def save_image(fn, font_size):
+def save_image(fn, sample_counter, font_size):
     img_x = Image.new("RGB", (256, 256), "white")
     draw_x = ImageDraw.Draw(img_x)
     draw_x.text((0, 0), text, "black", font=get_font(font_size))
-    img_x.save("{0}/{1}_{2}.png".format(DATA_DIR, fn, font_size))
+    img_x.save("{0}/{1}_{2}.png".format(DATA_DIR, fn, sample_counter))
 
+sample_counter = 0
 for font_size in FONT_SIZE_RANGE:
     text = "Sample Text"
-    save_image("x", font_size)
-    save_image("y", TARGET_FONT_SIZE)
+    save_image("x", sample_counter, font_size)
+    save_image("y", sample_counter, TARGET_FONT_SIZE)
+    sample_counter += 1
